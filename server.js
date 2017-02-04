1dc94ff164166459500f189eaf459233e245b986
indranil bhat <indranil2495b@users.noreply.github.com>
2017-02-04 12:53:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -77,7 +77,7 @@ app.get('/', function (req, res) {
 
 app.get('/article-one', function(req, res)
 {
-    res.send(createTemplate(articleOne));
+    res.send(htmlTemplate(articleOne));
 });
 
 app.get('/article-two', function(req, res)
