8e292d5b09d80d89f332063014534611fc4ed325
indranil bhat <indranil2495b@users.noreply.github.com>
2017-02-06 21:29:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,12 +3,10 @@
 var button = document.getElementById('counter');
 
 
-//requset
-
 
 button.onclick = function()
 {
-    //make a request to the counter and capture the response
+   
     
    var request = new XMLHttpRequest();
    
@@ -21,12 +19,12 @@ request.onreadystatechange = function ()
         {
             var counter = request.responseText;
              var span = document.getElementById('count');
-             displayCount.innerHTML = counter.toString();
+             span.innerHTML = counter.toString();
             
         }
     }
 };
-//make a request 
+
 
 request.open('GET','http://indranil2495b@ssh.imad.hasura-app.io/counter', true);
 request.send(null);
