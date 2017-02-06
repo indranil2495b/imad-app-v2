da5c01ebc6ac1a69272c840a4382656ea5a0edae
indranil bhat <indranil2495b@users.noreply.github.com>
2017-02-06 20:50:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,25 +1,14 @@
-console.log('Loaded!');
+//counter code
+
+var button = document.getElementById('counter');
+var counter = 0;
 
 
-//change the text of the main-text div
 
-var element = document.getElementById('main-text');
-
-element.innerHTML = 'Indranil Bhattacharji';
-
-
-//move the image
-
-var img = document.getElementById('madi');
-
-var marginLeft = 0;
-function moveRight ()
+button.onclick = function()
 {
-    marginLeft = marginLeft + 1;
-    img.style.marginLeft = marginLeft + 'px';
+    //make a request to the counter and capture the response
+    counter = counter + 1;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
 }
-img.onclick = function() 
-{
-    var interval = setInterval(moveRight, 50);
-    
-};
