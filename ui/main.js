035f11fbe5190fad1d03d6d8042e65db629ef992
indranil bhat <indranil2495b@users.noreply.github.com>
2017-02-07 21:43:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -33,8 +33,7 @@ request.send(null);
 };
 
 //submit name
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
+
 var submit = document.getElementById('submit_btn');
 submit.onclick = function ()
 {
@@ -61,6 +60,8 @@ request.onreadystatechange = function ()
     }
 };
 
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
 
 request.open('GET','http://indranil2495b.imad.hasura-app.io/submit-name?name=' + name, true);
 request.send(null);
