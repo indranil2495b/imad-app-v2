47b52fca0f554ab06f1b4a2d788e7cb00fdbf5fd
indranil bhat <indranil2495b@users.noreply.github.com>
2017-02-07 21:41:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -38,7 +38,17 @@ var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function ()
 {
-    var names = ['name1','name2','name3','name4','name5'];
+    var request = new XMLHttpRequest();
+   
+   
+request.onreadystatechange = function ()
+{
+    if(request.readystate === XMLHttpRequest.DONE)
+    {
+        if(request.status === 200)
+        {
+             var names = request.responseText;
+             names= JSON.parse(names);
     var list = ' ';
     for(var i=0;i< names.length;i++)
     {
@@ -46,6 +56,17 @@ submit.onclick = function ()
     }
     var ul = document.getElementById('namelist');
     ul.innerHTML = list;
+            
+        }
+    }
+};
+
+
+request.open('GET','http://indranil2495b.imad.hasura-app.io/submit-name?name=' + name, true);
+request.send(null);
+
+    
+   
 };
 
 
